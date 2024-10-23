a8a8af1435eb9cdd3db54652cbb13c928249e199
Adaias <AdaiasSouza>
2024-10-23 17:07:58 -0300

    Orientacao a Objeto

---
+++ Poo/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+    Programação Orientada a Objeto: Classes, Atributos, Herança, Método
+    Polimorfismo, Encapsulamento(getters, setters), Interface...
+
+*/
+using System;
+
+namespace Payments{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Programando!");
+
+        // atributos
+        // metodos
+        }
+    }
+
+    class Customer{
+        string name;
+    }
+}
