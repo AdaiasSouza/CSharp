ac7feb444ed50e33e34c6acf0e76b55f3bc8cf2b
Adaias <AdaiasSouza>
2024-10-23 15:39:13 -0300

    atualização do menu

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -11,28 +11,35 @@ namespace Calculadora{
 
         static void Menu(){
 
-            Console.WriteLine("Informe um numero: ");
-            float num1 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Informe um numero: ");
-            float num2 = float.Parse(Console.ReadLine());
+            bool menuPrincipal = true;
+            while(menuPrincipal){
+                Console.WriteLine("Seleciona um opção: ");
+                Console.WriteLine("\t1 - Soma ");
+                Console.WriteLine("\t2 - Subtração ");
+                Console.WriteLine("\t3 - Multiplicação ");
+                Console.WriteLine("\t4 Divisão ");
 
-            Console.WriteLine("Seleciona um opção: ");
-            Console.WriteLine("\t1 - Soma ");
-            Console.WriteLine("2 - Subtração ");
-            Console.WriteLine("3 - Multiplicação ");
-            Console.WriteLine("4 Divisão ");
+                short opcao = short.Parse(Console.ReadLine());                
+                
+                switch(opcao){
+                    case 1: OpcaoSoma(); break;
+                    case 2: OpcaoSubtracao(); break;                    
+                    case 3: OpcaoMultiplicacao(); break;
+                    case 4: OpcaoDivisao(); break;
+                    default: Console.WriteLine("Opcao inválida!"); break;
+                     
+                }
 
-            short opcao = short.Parse(Console.ReadLine());
+                Console.WriteLine("Voltar ao menu principal ?");
+                char voltaMenuPrincipal = char.Parse(Console.ReadLine());
+                if(voltaMenuPrincipal != 'S'){
+                            menuPrincipal = false;
+                } 
+                
+            } // fim menu principal        
 
-            switch(opcao){
-                case 1: Console.WriteLine("Soma: "+Soma(num1, num2)); break;
-                case 2: Console.WriteLine("Soma: "+Subtracao(num1, num2)); break;
-                case 3: Console.WriteLine("Soma: "+Multiplicacao(num1, num2)); break;
-                case 4: Console.WriteLine("Soma: "+Divisao(num1, num2)); break;
-                default: Menu(); break;
-            }
         }
-
+        // metodos
         static float Soma(float num1, float num2){
             return (num1 + num2);
         }
@@ -51,5 +58,75 @@ namespace Calculadora{
             }
         }        
 
-    }
+        static void OpcaoSoma(){
+            bool opcaoMenu = true;
+                    while(opcaoMenu){
+                        Console.WriteLine("Informe um numero: ");
+                        float num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe um numero: ");
+                        float num2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Soma: "+Soma(num1, num2));
+
+                        Console.WriteLine("Efetuar nova subtração ? S  -> Sim N -> Nao");
+                        char opcaoSelecao = char.Parse(Console.ReadLine());
+                        if(opcaoSelecao != 'S'){
+                            opcaoMenu = false;
+                        }                    
+                    } // fim while                     
+        } // fim OpcaoSoma
+
+        static void OpcaoSubtracao(){
+            bool opcaoMenu = true;
+                    while(opcaoMenu){
+                        Console.WriteLine("Informe um numero: ");
+                        float num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe um numero: ");
+                        float num2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Subtracao: "+Subtracao(num1, num2));
+
+                        Console.WriteLine("Efetuar nova subtração ? S  -> Sim N -> Nao");
+                        char opcaoSelecao = char.Parse(Console.ReadLine());
+                        if(opcaoSelecao != 'S'){
+                            opcaoMenu = false;
+                        }                    
+                    } // fim while 
+        } // fim OpcaoSubtracao
+
+        static void OpcaoMultiplicacao(){
+            bool opcaoMenu = true;
+                    while(opcaoMenu){
+                        Console.WriteLine("Informe um numero: ");
+                        float num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe um numero: ");
+                        float num2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Multiplicacao: "+Multiplicacao(num1, num2));
+
+                        Console.WriteLine("Efetuar nova multiplicacao ? S  -> Sim N -> Nao");
+                        char opcaoSelecao = char.Parse(Console.ReadLine());
+                        if(opcaoSelecao != 'S'){
+                            opcaoMenu = false;
+                        }                    
+                    } // fim while 
+
+        }
+
+        static void OpcaoDivisao(){
+            bool opcaoMenu = true;
+                    while(opcaoMenu){
+                        Console.WriteLine("Informe um numero: ");
+                        float num1 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Informe um numero: ");
+                        float num2 = float.Parse(Console.ReadLine());
+                        Console.WriteLine("Divisao: "+Divisao(num1, num2));
+
+                        Console.WriteLine("Efetuar nova divisao ? S  -> Sim N -> Nao");
+                        char opcaoSelecao = char.Parse(Console.ReadLine());
+                        if(opcaoSelecao != 'S'){
+                            opcaoMenu = false;
+                        }                    
+                    } // fim while 
+        }
+        
+        
+    } // fim Operacoes
 }
